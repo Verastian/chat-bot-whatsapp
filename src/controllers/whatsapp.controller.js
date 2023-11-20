af3ca013d58e80d3e7e2e35c0f895a4487af2b65
verastian <verastian0908@gmail.com>
2023-11-19 21:00:13 -0300

    feature: changes in verifyToken

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -1,17 +1,19 @@
 const verifyToken = (req, res) => {
+    console.log(req.query)
+    console.log(req.body)
     try {
         const accessToken = "affadfsuf9af9sdf!sSJkijjkDKSJ8i9h9hsd9u767sd8SHuSI";
         const token = req.query["hub.verify_token"];
         const challenge = req.body["hub.challenge"];
 
-        if (challenge != null && token != null && token === accessToken) {
+        if (challenge !== null && token !== null && token === accessToken) {
             res.send(challenge);
         } else {
-            res.status(400).send();
+            res.status(400).json({ message: 'error al verificar token' });
         }
 
     } catch (error) {
-        res.status(400).send();
+        res.status(400).json({ message: 'no se pudo verificar token' });
     }
 }
 
