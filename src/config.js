6f249e6f0231898818fe01bc71b26b8baf4cd9cd
verastian <verastian0908@gmail.com>
2024-03-20 21:30:19 -0300

    fix:se corrige variable de entorno para ACCESS_TOKEN

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ module.exports = {
     TOKEN_WHATSAPP_API: process.env.TOKEN_WHATSAPP_API,
     APP_ID: process.env.APP_ID || 'v18.0',
     APP_VERSION: process.env.APP_VERSION,
-    ACCESS_TOKEN: ACCESS_TOKEN,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
 }
